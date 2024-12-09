cebc703df56d3671cb3b04b112148474fa59dbc3
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2024-12-10 01:43:18 +0300

    chore: separate dynamic imports

--- aiostdlib/internal/backports/typing.py
+++ aiostdlib/internal/backports/typing.py
@@ -7,9 +7,15 @@ __all__: list[str] = ["ParamSpec", "Self", "TypeAlias"]
 
 
 if sys.version_info >= (3, 10):
-    from typing import ParamSpec, TypeAlias
+    from typing import ParamSpec
 else:
-    from typing_extensions import ParamSpec, TypeAlias
+    from typing_extensions import ParamSpec
+
+
+if sys.version_info >= (3, 10):
+    from typing import TypeAlias
+else:
+    from typing_extensions import TypeAlias
 
 
 if sys.version_info >= (3, 11):
