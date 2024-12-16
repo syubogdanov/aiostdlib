e191a3cada71de2badf4b1e409ced390b3f09e91
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2024-12-16 21:14:46 +0300

    fix: change the import path

--- aiostdlib/internal/backports/json/__init__.py
+++ aiostdlib/internal/backports/json/__init__.py
@@ -5,13 +5,8 @@ See Also
 * `json`.
 """
 
-from aiostdlib.internal.backports.json.py313 import (
-    JSONDecodeError,
-    JSONDecoder,
-    JSONEncoder,
-    dumps,
-    loads,
-)
+from aiostdlib.internal.backports.json.py313 import JSONDecoder, JSONEncoder, dumps, loads
+from aiostdlib.internal.backports.json.src.errors import JSONDecodeError
 
 
 __all__: list[str] = ["JSONDecodeError", "JSONDecoder", "JSONEncoder", "dumps", "loads"]
