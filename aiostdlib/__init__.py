101b1394b34666cd969c6228e3f114f1bd2e1e04
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2024-12-17 02:01:31 +0300

    feat: extend __init__.py

---
+++ aiostdlib/__init__.py
@@ -0,0 +1,13 @@
+from aiostdlib import builtins, json, pathlib, shutil, tarfile, tempfile, tomllib, zipfile
+
+
+__all__: list[str] = [
+    "builtins",
+    "json",
+    "pathlib",
+    "shutil",
+    "tarfile",
+    "tempfile",
+    "tomllib",
+    "zipfile",
+]
