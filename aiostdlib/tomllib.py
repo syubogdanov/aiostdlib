e31ddb198ab43bda729af11465b45ec1ba4c0e45
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2024-12-30 14:51:15 +0300

    feat: mock __module__

--- aiostdlib/tomllib.py
+++ aiostdlib/tomllib.py
@@ -2,3 +2,9 @@ from aiostdlib.internal.impl.tomllib import TOMLDecodeError, load, loads
 
 
 __all__: list[str] = ["TOMLDecodeError", "load", "loads"]
+
+
+TOMLDecodeError.__module__ = __name__
+
+load.__module__ = __name__
+loads.__module__ = __name__
