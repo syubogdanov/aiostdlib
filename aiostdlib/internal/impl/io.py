bab46dbc77dce4289162a172f54d2b7e4fbed27d
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2025-01-02 00:33:32 +0300

    feat: add the stubs

--- aiostdlib/internal/impl/io.py
+++ aiostdlib/internal/impl/io.py
@@ -5,6 +5,7 @@ from aiostdlib.internal.backports.io import (
     text_encoding,
 )
 from aiostdlib.internal.backports.os import SEEK_CUR, SEEK_SET
+from aiostdlib.internal.backports.typing import Self
 
 
 class IOBase:
@@ -13,4 +14,112 @@ class IOBase:
     See Also
     --------
     * `io.IOBase`.
+    """
+
+
+class RawIOBase:
+    """The base class for raw binary streams.
+
+    See Also
+    --------
+    * `io.RawIOBase`.
+    """
+
+
+class BufferedIOBase:
+    """The base class for binary streams that support some kind of buffering.
+
+    See Also
+    --------
+    * `io.BufferedIOBase`.
+    """
+
+
+class FileIO:
+    """A raw binary stream representing an OS-level file containing bytes data.
+
+    See Also
+    --------
+    * `io.FileIO`.
+    """
+
+
+class BytesIO:
+    """A binary stream using an in-memory bytes buffer.
+
+    See Also
+    --------
+    * `io.BytesIO`.
+    """
+
+
+class BufferedReader:
+    """A buffered binary stream.
+
+    See Also
+    --------
+    * `io.BufferedReader`.
+    """
+
+
+class BufferedWriter:
+    """A buffered binary stream.
+
+    See Also
+    --------
+    * `io.BufferedWriter`.
+    """
+
+
+class BufferedRandom:
+    """A buffered binary stream.
+
+    See Also
+    --------
+    * `io.BufferedRandom`.
+    """
+
+
+class BufferedRWPair:
+    """A buffered binary stream.
+
+    See Also
+    --------
+    * `io.BufferedRWPair`.
+    """
+
+
+class TextIOBase:
+    """The base class for text streams.
+
+    See Also
+    --------
+    * `io.TextIOBase`.
+    """
+
+
+class TextIOWrapper:
+    """A buffered text stream.
+
+    See Also
+    --------
+    * `io.TextIOWrapper`.
+    """
+
+
+class StringIO:
+    """A text stream using an in-memory text buffer.
+
+    See Also
+    --------
+    * `io.StringIO`.
+    """
+
+
+class IncrementalNewlineDecoder:
+    """A helper codec that decodes newlines for universal newlines mode.
+
+    See Also
+    --------
+    * `io.IncrementalNewlineDecoder`.
     """
